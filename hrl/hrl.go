a7a66e78fa869b252e5d2a50ee11f2011d010488
Alexandre Beslic <alexandre.beslic@gmail.com>
2014-08-21 17:07:32 +0200

    Added flag management for timer configuration

--- hrl/hrl.go
+++ hrl/hrl.go
@@ -16,7 +16,7 @@ import (
 
 type Pomodoro struct {
 	conn        *db.DB
-	round       time.Duration
+	timer       time.Duration
 	shortBreak  time.Duration
 	longBreak   time.Duration
 	totalRounds int
@@ -39,10 +39,13 @@ func checkError(err error) {
 }
 
 func main() {
-	// TODO Put into HOME directory in .heirloom
-	dbDir := "/tmp/heirloom"
-	// os.RemoveAll(dbDir)
-	// defer os.RemoveAll(dbDir)
+	dbDir := os.Getenv("HOME") + "/.heirloom"
+
+	dbExist := true
+	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
+		fmt.Println("Database directory does not exist yet, creating: %s", dbDir)
+		dbExist = false
+	}
 
 	conn, err := db.OpenDB(dbDir)
 	if err != nil {
@@ -52,49 +55,72 @@ func main() {
 
 	pomodoro := Pomodoro{
 		conn:        conn,
-		round:       time.Minute * 20,
+		timer:       time.Minute * 20,
 		shortBreak:  time.Minute * 5,
-		longBreak:   time.Minute * 20,
+		longBreak:   time.Minute * 25,
 		totalRounds: 5,
 		current:     1,
 		currentTask: nil,
 	}
 
-	pomodoro.initDB()
+	if !dbExist {
+		pomodoro.initDB()
+	}
 
 	app := cli.NewApp()
 	app.Name = "hrl"
 	app.Usage = "Simple command line tool for the Pomodoro® technique"
 	app.Version = "0.0.1"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "timer, t",
-			Value: "25",
-			Usage: "time for a single pomodoro round",
-		},
-		cli.StringFlag{
-			Name:  "short, s",
-			Value: "5",
-			Usage: "time for a short break",
-		},
-		cli.StringFlag{
-			Name:  "long, l",
-			Value: "20",
-			Usage: "time for a long break",
-		},
-		cli.StringFlag{
-			Name:  "rounds, r",
-			Value: "1",
-			Usage: "number of pomodoro rounds",
-		},
-	}
 	app.Commands = []cli.Command{
 		{
-			Name:      "start",
-			ShortName: "s",
-			Usage:     "Start a pomodoro timer",
+			Name:  "start",
+			Usage: "Start a pomodoro timer",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "timer, t",
+					Value: "25",
+					Usage: "time for a single pomodoro round",
+				},
+				cli.StringFlag{
+					Name:  "short, s",
+					Value: "5",
+					Usage: "time for a short break",
+				},
+				cli.StringFlag{
+					Name:  "long, l",
+					Value: "20",
+					Usage: "time for a long break",
+				},
+				cli.StringFlag{
+					Name:  "rounds, r",
+					Value: "1",
+					Usage: "number of pomodoro rounds",
+				},
+			},
 			Action: func(c *cli.Context) {
-				// TODO handle cli flags
+				if c.String("timer") != "" {
+					timer, err := strconv.Atoi(c.String("timer"))
+					checkError(err)
+					pomodoro.SetTimer(timer)
+				}
+
+				if c.String("short") != "" {
+					short, err := strconv.Atoi(c.String("short"))
+					checkError(err)
+					pomodoro.SetShortBreak(short)
+				}
+
+				if c.String("long") != "" {
+					long, err := strconv.Atoi(c.String("long"))
+					checkError(err)
+					pomodoro.SetLongBreak(long)
+				}
+
+				if c.String("rounds") != "" {
+					rounds, err := strconv.Atoi(c.String("rounds"))
+					checkError(err)
+					pomodoro.SetRounds(rounds)
+				}
 
 				round := make(chan bool)
 				smallBreak := make(chan bool)
@@ -108,16 +134,11 @@ func main() {
 			},
 		},
 		{
-			Name:      "add",
-			ShortName: "a",
-			Usage:     "Add a task to the collection of tasks to accomplish",
+			Name:  "add",
+			Usage: "Add a task to the collection of tasks to accomplish",
 			Action: func(c *cli.Context) {
 				desc := c.Args().First()
-
-				uuid, err := uuidgen.NewV4()
-				checkError(err)
-
-				pomodoro.insertTask(uuid.String(), desc, nil)
+				pomodoro.insertTask(desc, nil)
 			},
 		},
 	}
@@ -127,12 +148,12 @@ func main() {
 func (p *Pomodoro) beginRound(chanPomodoro chan bool) {
 	p.pickTask()
 	if p.currentTask == nil {
-		fmt.Println("No more tasks in collection: You are on your own! Or stop the round and add a new task with `hrl add`")
+		fmt.Println("No more tasks in collection: You are on your own!")
 	} else {
 		fmt.Println("Current Task: " + p.currentTask.description)
 	}
 	p.tellBeginRound()
-	time.Sleep(p.round)
+	time.Sleep(p.timer)
 	p.tellEndRound()
 	chanPomodoro <- true
 }
@@ -140,12 +161,14 @@ func (p *Pomodoro) beginRound(chanPomodoro chan bool) {
 func (p *Pomodoro) beginBreak(chanBreak chan bool) {
 	tellBeginSmallBreak()
 	time.Sleep(p.shortBreak)
+	tellEndSmallBreak()
 	chanBreak <- true
 }
 
 func (p *Pomodoro) beginLongBreak(chanLongBreak chan bool) {
 	tellBeginLongBreak()
 	time.Sleep(p.longBreak)
+	tellEndLongBreak()
 	chanLongBreak <- true
 }
 
@@ -159,8 +182,6 @@ func (p *Pomodoro) service(chanPomodoro, chanBreak, chanLongBreak, chanDone chan
 			if p.current >= p.totalRounds {
 				go p.beginLongBreak(chanLongBreak)
 				p.current = 1
-				// TODO Ask if continue?
-				chanDone <- true
 			} else {
 				p.current += 1
 				go p.beginBreak(chanBreak)
@@ -168,13 +189,19 @@ func (p *Pomodoro) service(chanPomodoro, chanBreak, chanLongBreak, chanDone chan
 
 		case smallBreak := <-chanBreak:
 			_ = smallBreak
-			tellEndSmallBreak()
 			go p.beginRound(chanPomodoro)
 
 		case longBreak := <-chanLongBreak:
 			_ = longBreak
-			tellEndLongBreak()
-			go p.beginRound(chanPomodoro)
+			input := askAnotherSession()
+			for input != "Y" && input != "N" {
+				input = askAnotherSession()
+			}
+			if input == "Y" && p.currentTask != nil {
+				go p.beginRound(chanPomodoro)
+			} else {
+				chanDone <- true
+			}
 
 		}
 	}
@@ -196,9 +223,6 @@ func (p *Pomodoro) tellEndRound() {
 	if input == "Y" && p.currentTask != nil {
 		p.deleteTask(p.currentTask.docId)
 		p.currentTask = nil
-		p.pickTask()
-	} else {
-		// TODO ask if want to continue with this task or pick another one
 	}
 }
 
@@ -219,19 +243,31 @@ func tellEndLongBreak() {
 }
 
 func askFinished() string {
-	fmt.Printf("Have you finished the current task? (Y/N)")
+	fmt.Println("Have you finished the current task? (Y/N)")
 	var input string
 	fmt.Scanln(&input)
 	return input
 }
 
-func (p *Pomodoro) insertTask(uuid string, description string, tags []string) error {
+func askAnotherSession() string {
+	fmt.Println("Ready for another pomodoro session? (Y/N)")
+	var input string
+	fmt.Scanln(&input)
+	return input
+}
+
+func (p *Pomodoro) insertTask(description string, tags []string) error {
 	tasks := p.conn.Use("Tasks")
 
-	_, err := tasks.Insert(map[string]interface{}{
-		"uuid": uuid,
+	uuid, err := uuidgen.NewV4()
+	checkError(err)
+
+	docId, err := tasks.Insert(map[string]interface{}{
+		"uuid": uuid.String(),
 		"desc": description,
 		"tags": []interface{}{}})
+
+	_ = docId
 
 	if err != nil {
 		panic(err)
@@ -244,7 +280,6 @@ func (p *Pomodoro) pickTask() {
 	tasks := p.conn.Use("Tasks")
 
 	tasks.ForEachDoc(func(id int, docContent []byte) (willMoveOn bool) {
-		// Take first task encountered
 		var doc map[string]interface{}
 		if json.Unmarshal(docContent, &doc) != nil {
 			panic("cannot deserialize")
@@ -267,13 +302,13 @@ func (p *Pomodoro) deleteTask(docId int) error {
 }
 
 func (p *Pomodoro) initDB() {
-	p.conn.Create("Tasks")
+	if err := p.conn.Create("Tasks"); err != nil {
+		panic(err)
+	}
 
 	tasks := p.conn.Use("Tasks")
 	tasks.Index([]string{"uuid"})
 	tasks.Index([]string{"desc"})
-
-	p.syncDB()
 }
 
 func (p *Pomodoro) closeDB() {
@@ -286,4 +321,20 @@ func (p *Pomodoro) syncDB() {
 	if err := p.conn.Sync(); err != nil {
 		panic(err)
 	}
+}
+
+func (p *Pomodoro) SetTimer(timer int) {
+	p.timer = time.Minute * time.Duration(timer)
+}
+
+func (p *Pomodoro) SetShortBreak(short int) {
+	p.shortBreak = time.Minute * time.Duration(short)
+}
+
+func (p *Pomodoro) SetLongBreak(long int) {
+	p.longBreak = time.Minute * time.Duration(long)
+}
+
+func (p *Pomodoro) SetRounds(rounds int) {
+	p.totalRounds = rounds
 }
